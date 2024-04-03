dc5453501a0f22b2e95bdfe46e795fc6a0da5ddc
Lucio Gonzalez <lucgosa2@gmail.com>
2024-04-03 09:52:58 +0200

    first commit

---
+++ destino.php
@@ -0,0 +1,30 @@
+<html>
+<head>
+</head>
+<body>
+<?php
+	echo "Nombre: " . $_POST["nombre"];
+	echo "</br>";
+	echo "Apellidos: " . $_POST["apellidos"];
+	echo "</br>";
+	echo "Fecha: " . $_POST["fecha"];
+	echo "</br>";
+	echo "Sexo: " . $_POST["sexo"];
+	echo "</br>";
+	echo "Nº de hijos: " . $_POST["num_hijos"];
+	echo "</br>";
+	echo "Email: " . $_POST["dir_email"];
+	echo "</br>";
+	echo "Teléfono: " . $_POST["telefono"];
+	echo "</br>";
+	echo "Dirección: " . $_POST["direccion"];
+	echo "</br>";
+	echo "Provincia: " . $_POST["provincia"];
+	echo "</br>";
+	echo "Comentario: " . $_POST["comentario"];
+	echo "</br>";
+	echo "Publicidad: " . $_POST["publicidad"];
+?>
+</body>
+</html>
+
